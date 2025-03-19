2935b88c37becaae9148d3a71cc372c9765876c2
Shehneen Fatima <shehneenfatima@gmail.com>
2025-03-19 16:01:55 +0500

    Added solution of Question 2 :Merge vowels of two arrays

---
+++ merge_vowels_array.py
@@ -0,0 +1,6 @@
+def alphabet_soup(s):
+    return "".join(sorted(s))
+
+# Example usage
+print(alphabet_soup("hello"))  # Expected Output: "ehllo"
+print(alphabet_soup("python"))  # Expected Output: "hnopty"
