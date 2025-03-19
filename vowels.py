240ab37d2b8cf824e7d9488044467842c124cfb3
Shehneen Fatima <shehneenfatima@gmail.com>
2025-03-19 15:16:45 +0500

    Added solution of Quetsion 1 : count and Reversed vowels

---
+++ vowels.py
@@ -0,0 +1,11 @@
+def count_vowels_reverse(s):
+    vowels = "aeiouAEIOU"
+    extracted_vowels = [char for char in s if char in vowels]
+    return len(extracted_vowels), extracted_vowels[::-1]
+
+# Example usage
+string = "Hello World"
+count, reversed_vowels = count_vowels_reverse(string)
+print("Vowel Count:", count)
+print("Reversed Vowels:", reversed_vowels)
+
