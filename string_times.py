46949807cd677707e8aeb5a3ba4c9425e06b5ac9
Shehneen Fatima <shehneenfatima@gmail.com>
2025-03-19 13:10:53 +0500

    Added solution for Question 4: Repeat String N Times

---
+++ string_times.py
@@ -0,0 +1,7 @@
+def string_times(string, n):
+    return string * n
+
+# Example usage
+print(string_times('Hi', 2))  # Output: HiHi
+print(string_times('Hi', 3))  # Output: HiHiHi
+print(string_times('Hi', 1))  # Output: Hi
